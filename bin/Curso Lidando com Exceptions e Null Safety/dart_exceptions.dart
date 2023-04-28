bbd26018f487fd06650b979845f8f2efa72caf6f
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-04-28 10:00:52 -0400

    03 - Criando Excessões Personalizadas - Criando Exceções com Exception

--- bin/Curso Lidando com Exceptions e Null Safety/dart_exceptions.dart
+++ bin/Curso Lidando com Exceptions e Null Safety/dart_exceptions.dart
@@ -3,38 +3,25 @@ import 'dart:io';
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
-// void main() {
-//   // Criando o banco
-//   BankController bankController = BankController();
-
-//   // Adicionando contas
-//   bankController.addAccount(
-//       id: "Ricarth",
-//       account:
-//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
-
-//   bankController.addAccount(
-//       id: "Kako",
-//       account:
-//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
-
-//   // Fazendo transferência
-//   bool result = bankController.makeTransfer(
-//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-//   // Observando resultado
-//   print(result);
-// }
-
 void main() {
-  try {
-    testFunc();
-  } on Exception catch (e) {
-    print(e.runtimeType);
-  } 
-  print("Nunca chegará aqui");
-}
+  // Criando o banco
+  BankController bankController = BankController();
 
-void testFunc(){
-  throw Account(name: "Kelvin", balance: 200, isAuthenticated: true);
+  // Adicionando contas
+  bankController.addAccount(
+      id: "Ricarth",
+      account:
+          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+
+  bankController.addAccount(
+      id: "Kako",
+      account:
+          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+
+  // Fazendo transferência
+  bool result = bankController.makeTransfer(
+      idSender: "Kako", idReceiver: "Ricarth", amount: 50);
+
+  // Observando resultado
+  print(result);
 }
---
+++ bin/Curso Lidando com Exceptions e Null Safety/exceptions/bank_controller_exceptions.dart
@@ -0,0 +1,15 @@
+class SenderIdInvalidException implements Exception {
+  
+}
+
+class ReceiverIdInvalidException implements Exception {
+  
+}
+
+class SenderNotAuthenticatedException implements Exception {
+  
+}
+
+class SenderBalanceLowerThaAmountException implements Exception {
+  
+}
