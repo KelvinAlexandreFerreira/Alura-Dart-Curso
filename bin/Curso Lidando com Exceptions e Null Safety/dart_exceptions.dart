e9075cba265190c8e6c4ebea421712d9347d6fd2
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-04-27 21:15:24 -0400

    02 - Tratamento de Excessões - Lidando com Exceções com o try-on

--- bin/Curso Lidando com Exceptions e Null Safety/dart_exceptions.dart
+++ bin/Curso Lidando com Exceptions e Null Safety/dart_exceptions.dart
@@ -32,15 +32,19 @@ void main(){
 
 void functionOne(){
   print("Started F01");
-  functionTwo();
+  try {
+    functionTwo();
+  } on FormatException {
+    print("A conversão não pôde ser feita.");
+  }  
   print("Finished F01"); 
 }
 
 void functionTwo(){
   print("Started F02");
   for (var i = 1 ; i < 5; i++) {
-    print(i);  
-    double amount = double.parse("Not a Number");
+    print(i); 
+    double amount = double.parse("Not a Number");  
   }
   print("Finished F02"); 
 }
