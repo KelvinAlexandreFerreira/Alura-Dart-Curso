a4930a7e6124eceef4e786797e3a1620fd9cc98c
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-05-01 12:56:46 -0400

    03 - Streams - Subscription

--- bin/Curso Dart - Entendendo Assincronismo/streams.dart
+++ bin/Curso Dart - Entendendo Assincronismo/streams.dart
@@ -1,13 +1,25 @@
-void main(){
+import 'dart:async';
+
+void main() {
   Stream myStream(int interval, [int? maxCount]) async* {
     int i = 1;
     while (i != maxCount) {
       print("Counting: $i");
       await Future.delayed(Duration(seconds: interval));
       yield i++;
-    }  
+    }
     print('The Stream is finhished');
   }
-  myStream(1,10);
-  print('Main is finhished');
+
+  StreamSubscription mySubscriber = myStream(1, 10).listen((event) {
+    if (event.isEven) {
+      print('This number is Even!');
+    }
+  }, onError: (e) {
+    print('An eror happende: $e');
+  }, onDone: () {
+    print('The subscriber is gone.');
+  });
+
+  print('Main is finished!');
 }
