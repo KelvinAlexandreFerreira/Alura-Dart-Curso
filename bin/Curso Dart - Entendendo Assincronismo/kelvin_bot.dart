4fabeab8da45394dfe461decbd37e7a71146d591
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-05-01 10:43:27 -0400

    02 - Future - Tempo de resposta

--- bin/Curso Dart - Entendendo Assincronismo/kelvin_bot.dart
+++ bin/Curso Dart - Entendendo Assincronismo/kelvin_bot.dart
@@ -2,6 +2,7 @@
 import 'dart:io';
 
 import 'questions/time_questions.dart';
+import 'timing/waiting_time.dart';
 
 void main() async {
   String kelvinBot = 'KelvinBOT:\n';
@@ -9,11 +10,13 @@ void main() async {
   String usuario = '';
 
   print('-- Iniciando o KelvinBOT, aguarde..--');
+  await BotClock().clock(2);
 
   print('KelvinBOT:\n Oi :) \n Como posso ajudar?');
   do {
     usuario = stdin.readLineSync().toString();
     print('-- Processando pergunta, aguarde..--');
+    await BotClock().clock(1);
     if (usuario.contains('xau') ||
         usuario.contains('Xau') ||
         usuario.contains('Adeus') ||
@@ -22,10 +25,12 @@ void main() async {
       print(kelvinBot + ' Até a proxima!!');
     } else if (TimeQuestions(usuario).isThisTime()) {
       // verificar antes, assim não fazemos toda a função sem precisar.
+      await BotClock().clock(2);
       TimeQuestions(usuario).timeQuestion();
     } else if (false) {
       //Basta adicionar novas perguntas aqui!
     } else {
+      await BotClock().clock(2);
       print(kelvinBot +
           ' Não fui treinado para responder a essa pergunta \n Desculpe :( ');
       print(kelvinBot + ' Você pode fazer outra pergunta ou dizer Adeus');
